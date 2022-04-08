794fdf6403cdc46bc223d81f35edba5fb9a19207
Sachin Varma <sachinvermaa1234@gmail.com>
2022-04-08 17:56:55 +0530

    set up Router

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,3 @@
 module.exports.home = function (req,res) { 
-    return res.end('<h1> setting routes </h1>')
+    return res.end('<h1> setting home routes </h1>')
 }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller')
 
-console.log("router loaded")
-
 router.get('/',homeController.home);
+router.use('/',require('./users'));
+router.use('/',require('./posts'));
+
+// for any further routes, access from here
+// router.use('/usersName',require('./users'));
 
 module.exports = router;
