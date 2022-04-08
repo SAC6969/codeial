045b02f2f85f4fe9fac5b585b2df03de7494587c
Sachin Varma <sachinvermaa1234@gmail.com>
2022-04-08 17:01:48 +0530

    Create the directory and setup server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express()
+const port = 8000
+
+
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log("error at express",err);
+    }
+    console.log(`Server is running on Port: ${port}`);
+})
